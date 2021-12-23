f53fb8137ddbf21d852356c9aec4f1909d309115
NakataVega <vegagerardo96@gmail.com>
2021-12-23 00:48:33 -0600

    Pages

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div>
+      <h1>App</h1>
+      <Router>
+        <div>
+          <Link to="/main">Ir a Main</Link>
+        </div>
+        <Switch>
+          <Route exact path="/">
+            Welcome
+          </Route>
+          <Route path="/main">
+            Main
+          </Route>
+          <Route path="/city">
+            City
+          </Route>
+          <Route>
+            Not Found
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  )
+}
+
+export default App
