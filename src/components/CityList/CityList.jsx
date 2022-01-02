dd7b7b7843e32ded9b43162be7e6e7942ce3a774
NakataVega <vegagerardo96@gmail.com>
2022-01-02 00:04:23 -0600

    Skeleton and TryCatch

--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -2,19 +2,22 @@ import React, {useState, useEffect} from 'react'
 import PropTypes from 'prop-types'
 import axios from 'axios'
 import convertUnits from 'convert-units'
+import Alert from '@material-ui/lab/Alert'
 import Grid from '@material-ui/core/Grid'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import CityInfo from './../CityInfo'
 import Weather from './../Weather'
 
+const getCityCode = (city, countryCode) => `${city}-${countryCode}`
+
 //renderCityAndCountry se va a convertir en una funcion que retorna otra funcion
 const renderCityAndCountry = eventOnClickCity => (cityAndCountry, weather) => {
-  const {city, country} = cityAndCountry
+  const {city, countryCode, country} = cityAndCountry
   //const {temperature, state} = weather
   return (
     <ListItem 
-      key={city} onClick={eventOnClickCity}
+      key={getCityCode(city, countryCode)} onClick={eventOnClickCity}
       button
     >
       <Grid container justifyContent="center" alignItems="center">
@@ -22,9 +25,7 @@ const renderCityAndCountry = eventOnClickCity => (cityAndCountry, weather) => {
           <CityInfo city={city} country={country}/>
         </Grid>
         <Grid item md={3} xs={12}>
-          {
-            weather ? (<Weather temperature={weather.temperature} state={weather.state}/>) : ("No hay datos")
-          }
+          <Weather temperature={weather && weather.temperature} state={weather && weather.state}/>
         </Grid>
       </Grid>
     </ListItem>
@@ -34,40 +35,51 @@ const renderCityAndCountry = eventOnClickCity => (cityAndCountry, weather) => {
 const CityList = ({cities, onClickCity}) => {
   const [allWeather, setAllWeather] = useState({})
 
+  const [error, setError] = useState(null)
+
   useEffect(() => {
-    const setWeather = (city, country, countryCode) => {
+    const setWeather = async (city, countryCode) => {
       const apikey = "989b01fae952aa5ac734a6bbf1cfbf6b"
       const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=${apikey}` 
-      axios.get(url).then((response) => {
+      
+      try {
+        const response = await axios.get(url)
         const {data} = response
         const temperature = parseInt(convertUnits(data.main.temp).from("K").to("C").toFixed(0))
         const state = data.weather[0].main.toLowerCase()
-        const propName = `${city}-${country}` // Ej: [Guadalajara-Mexico]
+        const propName = getCityCode(city, countryCode) // Ej: [Guadalajara-MX]
         const propValue = { temperature, state} //Ej: {temperature: 10, state:"sunny"}
         
-
-
-        setAllWeather(allWeather => (
-          {...allWeather, [propName]: propValue }
-        ))
-
-      })
-      
+        setAllWeather(allWeather => ({...allWeather, [propName]: propValue}))
+      } catch (error) {
+        if(error.response){ //El server no responde
+          const {data, status} = error.response
+          setError("Error con el servidor")
+        } else if (error.request) { //No llegar al server
+          setError("Verifique la conexión a Internet")
+        } else { //Razones imprevistas
+          setError("Error imprevisto")
+        }
+      }
     }
 
-    cities.forEach( ({city, country, countryCode})  => {
-      setWeather(city, country, countryCode)
+    cities.forEach( ({city, countryCode})  => {
+      setWeather(city, countryCode)
     })
 
   }, [cities])
 
-  //const weather = { temperature: 12, state:"sunny"}
   return (
-    <List>
+    <div>
       {
-        cities.map(cityAndCountry => renderCityAndCountry(onClickCity)(cityAndCountry, allWeather[`${cityAndCountry.city}-${cityAndCountry.country}`]))
+        error && <Alert severity='error' onClose={() => setError(null)}>{error}</Alert>
       }
-    </List>
+      <List>
+        {
+          cities.map(cityAndCountry => renderCityAndCountry(onClickCity)(cityAndCountry, allWeather[getCityCode(cityAndCountry.city, cityAndCountry.countryCode)]))
+        }
+      </List>
+    </div>
   )
 }
 
